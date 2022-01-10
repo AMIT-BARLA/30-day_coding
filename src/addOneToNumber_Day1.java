5d59ff4ca6282f5b6c5bb9b7dc76bacc463708b3
Amit Barla <amitbarla1997@gmail.com>
2022-01-11 00:34:40 +0530

    code for add one to number v1

--- src/addOneToNumber_Day1.java
+++ src/addOneToNumber_Day1.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 public class addOneToNumber_Day1 {
@@ -15,28 +14,27 @@ public class addOneToNumber_Day1 {
         obj.plusOne(a);
         System.out.println(a);
     }
-        public void plusOne(ArrayList<Integer> A) {
+    public void plusOne(ArrayList<Integer> A) {
         // Check if index at '0' has a value 0.
-            while(A.size() > 1 && A.get(0) == 0){
-                A.remove(0);
+        while(A.size() > 1 && A.get(0) == 0){
+            A.remove(0);
+        }
+        int rem = 0;
+        for(int i = A.size()-1; i>=0; i--){
+            int num = A.get(i);
+            A.remove(i);
+            if(num == 9){
+                A.add(i,0); // removing the 9 and adding 0 at this place.
+                rem = 1 ;
             }
-            int rem = 0;
-            for(int i = A.size()-1; i>=0; i--){
-                int num = A.get(i);
-                A.remove(i);
-                if(num == 9){
-                    A.add(i,0); // removing the 9 and adding 0 at this place.
-                    rem = 1 ;
-                }
-                else{
-                    A.add(i,num+1);
-                    rem = 0;
-                    break;
-                }
-            }
-            if( rem == 1){ // example:- 9999 So, all will become '0' and 1 will be added in the index '0'
-                A.add(0,1);
+            else{
+                A.add(i,num+1);
+                rem = 0;
+                break;
             }
         }
+        if( rem == 1){ // example:- 9999 So, all will become '0' and 1 will be added in the index '0'
+            A.add(0,1);
+        }
     }
-
+}
