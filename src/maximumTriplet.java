cc7dfd5003626d1f42b5dd3f43486153760920e7
Amit Barla <amitbarla1997@gmail.com>
2022-01-13 22:28:56 +0530

    day 4

--- src/maximumTriplet.java
+++ src/maximumTriplet.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class maximumTriplet {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -9,30 +12,48 @@ public class maximumTriplet {
             a1.add(i, in.nextInt());
         }
         int max = maxTriplet(a1);
-        System.out.println(max);
+        System.out.println("max = "+max);
     }
     public static int maxTriplet(ArrayList<Integer> a){
-        int[] arr = new int[a.size()];
-        System.out.println("length ="+arr.length);
-        int right = a.size()-2;
-        int currMax = a.get(a.size()-1);
-        arr[a.size()-1] = currMax;
-        while(right >= 0){
-            if (a.get(right) >= currMax ){
-                currMax = a.get((right));
-                arr[right]= currMax;
+
+        ArrayList<Integer> rightMax = new ArrayList<>();
+        int end = a.size()-2;
+        rightMax.add(0,a.get((a.size()-1)));
+        while(end >0){
+            System.out.println("rightMax = "+rightMax.get(0)+"end="+end +"A end ="+a.get(end));
+            if(rightMax.get(0) < a.get(end)){
+                rightMax.add(0,a.get(end));
+            } else {
+                rightMax.add(0, rightMax.get(0));
             }
-            arr[right]= currMax;
-            right--;
+            end--;
         }
-        for (int j = 0; j < arr.length; j++) {
-            System.out.print(arr[j] + " ");
+        System.out.println(rightMax);
+        Stack<Integer> s = new Stack<>();
+        end = a.size()-1;
+        while(end >=0){
+            s.push(a.get(end));
+            end--;
         }
-        for(int i = 0; i< a.size();i++){
-            int val = a.get(i);
-
+        System.out.println(s);
+        end = a.size()-1;
+        int start;
+        int currSum = 0;
+        int currIndex = 0;
+        while(!s.isEmpty()) {
+            start = currIndex;
+            while (start < end) {
+                int top = s.peek();
+                if (top < a.get(start + 1) && a.get(start + 1) < rightMax.get(start)) {
+                    currSum = Math.max(top + a.get(start + 1) + rightMax.get(start), currSum);
+                }
+                start++;
+            }
+            currIndex++;
+            s.pop();
         }
-
-        return 1;
+        return currSum;
     }
+    //21882, 31999, 10323, 18652, 10022, 5700, 3558, 28477, 27893, 24390, 5076, 10713, 2601, 2511, 21004, 26870
+    //73142
 }
